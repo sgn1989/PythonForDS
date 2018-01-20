c32a75a4750eb5d1b455128113adf9ea0883e0bd
Saptha <sgn1989@gmail.com>
2018-01-20 15:04:10 -0800

    Combined STEP 2 and STEP 3 sub-blocks.

--- scores_to_rating.py
+++ scores_to_rating.py
@@ -1,4 +1,4 @@
-def scores_to_rating(score1, score2, score3, score4, score5):
+def scores_to_rating(score1,score2,score3,score4,score5):
     """
     Turns five scores into a rating by averaging the middle three of the five
     scores and assigning this average to a written rating.
@@ -10,11 +10,8 @@ def scores_to_rating(score1, score2, score3, score4, score5):
     score4 = convert_to_numeric(score4)
     score5 = convert_to_numeric(score5)
 
-    # STEP 2 find middle three scores
-    #how am I going to do this?
-
-    # STEP 3 take average of middle three scores
-    average_score = # (sum of the middle scores)/3
+    # STEP 2 and STEP 3 find the average of the middle three scores
+    average_score = sum_of_middle_three(score1,score2,score3,score4,score5)/3
 
     # STEP 4 trun average score into a rating
     rating = score_to_rating_string(average_score)
