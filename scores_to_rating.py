077f86a9c70c70f1635df09fa0142a948ea437ca
Saptha <sgn1989@gmail.com>
2018-01-20 14:46:15 -0800

    Adding the skeleton of score_to_rating function.

---
+++ scores_to_rating.py
@@ -0,0 +1,8 @@
+def scores_to_rating(score1, score2, score3, score4, score5):
+    """
+    Turns five scores into a rating by averaging the middle three of the five
+    scores and assigning this average to a written rating.
+    """
+    # THE CODE GOES HERE
+
+    return rating
