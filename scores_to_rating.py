ddc108691225f2895a8b728b33dc0245491aadcd
Saptha <sgn1989@gmail.com>
2018-01-20 15:26:26 -0800

    Added outline of the helper functions.

--- scores_to_rating.py
+++ scores_to_rating.py
@@ -1,3 +1,25 @@
+def convert_to_numeric(score):
+    """
+    Covert score to a numerical type.
+    """
+    converted_score = #convert the score
+    return converted_score
+
+def sum_of_middle_three(score1,score2,score3,score4,score5):
+    """
+    Find the sum of the middle three numbers out of the five given.
+    """
+    sum = #add them together and take away the max and min
+    return sum
+
+def score_to_rating_string(score):
+    """
+    Convert the average score, which should be between 0 and 5,
+    into a string rating.
+    """
+    rating =
+    return rating
+
 def scores_to_rating(score1,score2,score3,score4,score5):
     """
     Turns five scores into a rating by averaging the middle three of the five
